3d63979eeff52ed50a4c3573d4e755a621db3c29
Geovane <geovaneviana.df@gmail.com>
2025-08-08 00:48:07 -0300

    #3 - Estrutura

---
+++ UniJG-Backend/Converters/CustomJsonConverterDateTimeNullable.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using UniJG.Application;
+using UniJG_Backend.Constants;
+
+namespace UniJG_Backend.Converters
+{
+    public class CustomJsonConverterDateTimeNullable : JsonConverter<DateTime?>
+    {
+        public override DateTime? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            try
+            {
+                DateTime dataInformada = DateTime.ParseExact(
+                    reader.GetString()[..Date.MaskLength],
+                    Date.MaskFormat,
+                    CultureInfo.InvariantCulture);
+
+                return TimeZoneInfo.ConvertTimeToUtc(
+                    dateTime: dataInformada,
+                    sourceTimeZone: TimeZoneInfo.FindSystemTimeZoneById(TimeZones.Brasilia));
+            } catch (Exception)
+            {
+                throw new JsonException("Data inválida");
+            }
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime? value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(WriteValue(value.Value));
+        }
+
+        internal static DateTimeOffset WriteValue(DateTime value)
+        {
+            TimeZoneInfo timezoneBrasilia = TimeZoneInfo.FindSystemTimeZoneById(TimeZones.Brasilia);
+            TimeSpan timeSpanBrasilia = timezoneBrasilia.GetUtcOffset(value);
+
+            value = value.Kind switch
+            {
+                DateTimeKind.Unspecified => TimeZoneInfo.ConvertTimeFromUtc(
+                    dateTime: value,
+                    destinationTimeZone: timezoneBrasilia),
+
+                DateTimeKind.Utc => TimeZoneInfo.ConvertTimeFromUtc(
+                    dateTime: value,
+                    destinationTimeZone: timezoneBrasilia),
+
+                DateTimeKind.Local => TimeZoneInfo.ConvertTimeFromUtc(
+                     dateTime: value.ToUniversalTime(),
+                     destinationTimeZone: timezoneBrasilia),
+
+                _ => value
+            };
+
+            return new DateTimeOffset(value, timeSpanBrasilia);
+        }
+    }
+}
